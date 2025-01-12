ced11bc64fce2fcdb31505b8394d4add33148427
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-01-12 18:59:14 +0530

    components linking
    linked various jsx files to app.jsx and presented the output

---
+++ components challenge/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./assets/components/header";
+import MainContent from "./assets/components/main content";
+import Footer from "./assets/components/footer";
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
