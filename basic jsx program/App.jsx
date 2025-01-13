6c75ffd4b9edf63e4c46c9d2477022413838dfd6
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-01-13 08:43:36 +0530

    basic
    basic

---
+++ basic jsx program/App.jsx
@@ -0,0 +1,9 @@
+import JSXRules from "./assets/components/jsx";
+const app = () =>{
+  return (
+    <div>
+      <JSXRules/>
+    </div>
+  );
+};
+export default app;
---
+++ expression/App.jsx
@@ -0,0 +1,12 @@
+const app=() =>{
+  const myname="my name is";
+  return(
+      <section>
+          <p>2+2={2+2}</p>
+          <h1>{myname}</h1>
+          <p>my friends list:{['alex','amrit','lisa']}</p>
+
+      </section>
+  );
+};
+export default app;
