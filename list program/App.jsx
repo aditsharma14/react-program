004dbfde56d847663d7977fa1683c1ea2897b897
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-01-14 23:37:53 +0530

    list program
    in this program I have made two lists

---
+++ list program/App.jsx
@@ -0,0 +1,12 @@
+import UserList from "./assets/components/list2";
+import ProductList from "./assets/components/list1";
+function app(){
+  return(
+    <div>
+      <UserList/>
+      <ProductList/>
+    </div>
+
+  );
+}
+export default app
