9c6b834febf1022956366eff26eaa0dfa99f69a5
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-01-25 00:10:37 +0530

    used useeffect with a condition
    will show increment (no.) as document title when no.>0

---
+++ useffect/useffect 1.jsx
@@ -0,0 +1,15 @@
+import { useEffect,useState } from "react";
+const App=()=>{
+    const [value,setvalue]=useState(0);
+    useEffect(()=>{
+        console.log("call useEffect");
+        document.title = `Increment (${value})`;
+    });
+    return(
+        <>
+            <h2>{value}</h2>
+            <button onClick={() => setvalue(value + 1)}>Click me</button>
+        </>
+    )
+};
+export default App;
